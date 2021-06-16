a27fb7215f47646cf8206b463093099e2852c35f
Daniel <darkkingyt55@gmail.com>
2021-06-16 09:44:15 -0500

    Mitad

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import React from "react";
+import "./App.css";
 
-import './App.css';
+function Helloworld(props) {
+  console.log(props)
+  return (
+  <div id="hello">
+    <h3>{props.subtitle}</h3>
+    {props.mytext}
+  </div>); 
+}
 
 function App() {
   return (
-    <div> Hello world</div>
+    <div>
+      This is my component:
+      <Helloworld mytext="Hello Daniel" subtitle="Proyecto" /> 
+      <Helloworld mytext="Hola Mundo" subtitle="Proyecto 2"/> 
+      <Helloworld mytext="Hello" subtitle="Proyecto 3"/>
+    </div>
   );
 }
 
